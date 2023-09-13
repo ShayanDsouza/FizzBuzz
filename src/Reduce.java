94d004dd599273ee24319d78d071e4fd325e85cf
Shayan Dsouza <dsouza.shayan@gmail.com>
2023-09-13 00:06:16 -0400

    task 5

--- src/Reduce.java
+++ src/Reduce.java
@@ -1,17 +1,17 @@
 public class Reduce {
     public static void main(String[] args) {
         int n = 100;
-        int steps = 0;
+        int count = 0;
 
         while (n > 0) {
             if (n % 2 == 0) {
-                n /= 2;  // If n is even, divide it by 2
+                n /= 2;
             } else {
-                n -= 1;  // If n is odd, subtract 1
+                n -= 1;
             }
-            steps++;
+            count++;
         }
 
-        System.out.println(steps);
+        System.out.println(count);
     }
 }
